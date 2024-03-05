fda80c8ca6aab97964074d700ca74adb888ded08
AddisonScott <159310467+AddisonScott@users.noreply.github.com>
2024-03-05 23:14:10 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,7 +9,7 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         //Current Value Property
-        private int CurrentValue
+        private int CurrentValue;
 
         
         //Method to roll dice and return integer
