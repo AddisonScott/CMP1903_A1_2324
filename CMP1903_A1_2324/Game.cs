92ad2db39c0959269f9f1ad0778c1687c092f1bb
AddisonScott <159310467+AddisonScott@users.noreply.github.com>
2024-03-06 03:14:06 +0000

    Update Game.cs
    final

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -6,16 +6,46 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Game
-    {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+   public class Game
+   {
 
-        //Methods
+       private Die[] dice;
+       public int RollNumber = 0;
 
-    }
+       //Creates 3 instance of die.
+       public Game()
+       {
+           dice = new Die[3];
+           for (int i = 0; i < 3; i++)
+           {
+               dice[i] = new Die();
+           }
+       }
+
+       //Generates a random number for each instance of die.
+       public int[] RollDie()
+       {
+           int[] rolls = new int[3];
+           for (int i = 0; i < 3; i++)
+           {
+               Console.WriteLine("Rolling....");
+               rolls[i] = dice[i].Roll();
+               Console.WriteLine($"Die number {RollNumber} is {rolls[i]}");
+               RollNumber++;
+           }
+           return rolls;
+       }
+
+       //Returns the sum of the 3 dice.
+       public int SumOfDice(int[] diceNumbers)
+       {
+           int sum = 0;
+           foreach (int i in diceNumbers)
+           {
+               sum += i;
+           }
+           return sum;
+       }
+   }
+
 }
